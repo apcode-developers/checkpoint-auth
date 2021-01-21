dc99c4afe91251bc035d10ede506b1021116adf6
hafizhkamus <hafizhkamus13@gmail.com>
2021-01-21 11:36:45 +0700

    auth service for checkpoint app

---
+++ src/test/java/com/check/point/service/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.check.point.service.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
